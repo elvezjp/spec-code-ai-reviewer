8b73c4bf5a3051500e04a02f9affc7871369a1ac
冨永善視 <yoshimi.tominaga@elvez.co.jp>
2026-02-06 16:38:24 +0900

    旧バージョンでの切り替えを更新

--- versions/v0.8.0/frontend/src/core/hooks/useVersions.ts
+++ versions/v0.8.0/frontend/src/core/hooks/useVersions.ts
@@ -8,9 +8,6 @@ const DEFAULT_VERSIONS: VersionInfo[] = [
   { value: 'v0.8.0', label: 'v0.8.0', isLatest: true },
   { value: 'v0.7.0', label: 'v0.7.0', isLatest: false },
   { value: 'v0.6.0', label: 'v0.6.0', isLatest: false },
-  { value: 'v0.5.2', label: 'v0.5.2', isLatest: false },
-  { value: 'v0.5.1', label: 'v0.5.1', isLatest: false },
-  { value: 'v0.5.0', label: 'v0.5.0', isLatest: false },
 ]
 
 // Cookieヘルパー
