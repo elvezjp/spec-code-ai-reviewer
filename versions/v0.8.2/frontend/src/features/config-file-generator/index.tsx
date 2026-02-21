eae0dd1b923916d964ec4997c637e3fd707a2474
冨永善視 <yoshimi.tominaga@elvez.co.jp>
2026-02-21 11:25:06 +0900

    v0.8.1からv0.8.2をコピー

---
+++ versions/v0.8.2/frontend/src/features/config-file-generator/index.tsx
@@ -0,0 +1,90 @@
+import { useCallback } from 'react'
+import { Layout, Header } from '@core/index'
+import { useConfigState } from './hooks/useConfigState'
+import { useValidation } from './hooks/useValidation'
+import {
+  InfoSection,
+  LlmSection,
+  SpecTypesTable,
+  SystemPromptsSection,
+  DownloadButton,
+} from './components'
+import { generateMarkdown, downloadMarkdown } from './services/markdownGenerator'
+import { CONFIG_SCHEMA } from './schema/configSchema'
+
+export function ConfigFileGenerator() {
+  const {
+    formState,
+    handleProviderChange,
+    handleLlmFieldChange,
+    handleArrayItemChange,
+    handleArrayItemAdd,
+    handleArrayItemRemove,
+    handleSpecTypeChange,
+    handleSpecTypeAdd,
+    handleSpecTypeRemove,
+    handleSystemPromptChange,
+    handleSystemPromptAdd,
+    handleSystemPromptRemove,
+  } = useConfigState()
+
+  const { validate, getFieldError } = useValidation()
+
+  const handleDownload = useCallback(() => {
+    const errors = validate(formState)
+    if (errors.length > 0) {
+      alert('入力内容にエラーがあります。赤字のメッセージを確認してください。')
+      return
+    }
+
+    const markdown = generateMarkdown(formState)
+    downloadMarkdown(markdown)
+  }, [formState, validate])
+
+  return (
+    <Layout>
+      {/* ヘッダー */}
+      <Header title="設定ファイルジェネレーター">
+        <p className="text-gray-600 text-sm text-center">
+          {CONFIG_SCHEMA.meta.outputTitle}を作成します。<br />
+          各項目を入力して「ダウンロード」ボタンを押してください。
+        </p>
+      </Header>
+
+      {/* info セクション */}
+      <InfoSection />
+
+      {/* llm セクション */}
+      <LlmSection
+        formState={formState}
+        onProviderChange={handleProviderChange}
+        onFieldChange={handleLlmFieldChange}
+        onArrayItemChange={handleArrayItemChange}
+        onArrayItemAdd={handleArrayItemAdd}
+        onArrayItemRemove={handleArrayItemRemove}
+        getFieldError={getFieldError}
+      />
+
+      {/* specTypes セクション */}
+      <SpecTypesTable
+        specTypes={formState.specTypes}
+        onSpecTypeChange={handleSpecTypeChange}
+        onSpecTypeAdd={handleSpecTypeAdd}
+        onSpecTypeRemove={handleSpecTypeRemove}
+      />
+
+      {/* systemPrompts セクション */}
+      <SystemPromptsSection
+        systemPrompts={formState.systemPrompts}
+        onSystemPromptChange={handleSystemPromptChange}
+        onSystemPromptAdd={handleSystemPromptAdd}
+        onSystemPromptRemove={handleSystemPromptRemove}
+      />
+
+      {/* ダウンロードボタン */}
+      <DownloadButton onDownload={handleDownload} />
+    </Layout>
+  )
+}
+
+export default ConfigFileGenerator
